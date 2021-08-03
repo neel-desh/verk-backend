36bfb8b5592e1eb091906a07d6963a45d5caae0c
Deepak Pai <deepak@market-pulse.in>
2021-08-03 10:03:01 +0530

    Changed redis config to support both keyword list and string, to support sentinel or plain redis

--- lib/verk/supervisor.ex
+++ lib/verk/supervisor.ex
@@ -31,7 +31,11 @@ defmodule Verk.Supervisor do
     shutdown_timeout = Confex.get_env(:verk, :shutdown_timeout, 30_000)
     generate_node_id = Confex.get_env(:verk, :generate_node_id)
 
-    redis = worker(Redix, [redis_url, [name: Verk.Redis]], id: Verk.Redis)
+    redis = case Keyword.keyword?(redis_url) do
+      false -> worker(Redix, [redis_url, [name: Verk.Redis]], id: Verk.Redis)
+      true -> worker(Redix, [redis_url ++ [name: Verk.Redis]], id: Verk.Redis)
+    end
+
     event_producer = worker(Verk.EventProducer, [], id: Verk.EventProducer)
     queue_stats = worker(Verk.QueueStats, [], id: Verk.QueueStats)
     schedule_manager = worker(Verk.ScheduleManager, [], id: Verk.ScheduleManager)
